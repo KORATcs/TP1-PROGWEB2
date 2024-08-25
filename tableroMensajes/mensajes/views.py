5fe84a242c3b0196c4273147f2914417fa58edf7
KORATcs <cgsimon@udc.edu.ar>
2024-08-25 00:13:13 -0300

    creacion de la vista

--- tableroMensajes/mensajes/views.py
+++ tableroMensajes/mensajes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Mensaje 
 
 # Create your views here.
+
+def mensajes (request):
+    recibidos =  Mensaje.objects.all()
+
+    return render(request, 'mensajes/mensajes.html', {'mensajes':recibidos})
